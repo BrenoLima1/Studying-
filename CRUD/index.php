9ec1e0f66e4b99b480d0b6379d57d9a45a89541f
BrenoLima1 <54006763+BrenoLima1@users.noreply.github.com>
2022-07-11 12:13:03 -0300

    Delete CRUD directory

--- CRUD/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-header( 'Content-Type: text/plain; charset=UTF-8' );
-http_response_code( 400 ); // Bad Request
-date_default_timezone_set( 'America/Sao_Paulo' );
-echo 'Bem-vindo, <b>Anônino</b>', '<br>', date('d/M/Y H:i:s');
-?>
